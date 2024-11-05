144c2857714b36cd0543ffe5a8bdccfe5ef77cc8
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-05 17:10:23 +0100

    ADD: button that fetches 10 more emails

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 const emailList = document.getElementById("email-list");
+const generateButton = document.getElementById("generate-btn");
 
-for (let i = 0; i < 10; i++) {
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(res => {
-            const email = res.data.response;
-            const liItem = document.createElement("li");
-            liItem.innerHTML = email;
-            emailList.appendChild(liItem);
+generateEmails();
 
-            console.log(email);
-        })
-        .catch(err => console.err(err));
-};
+function generateEmails() {
+
+    emailList.innerHTML = "";
+
+    for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(res => {
+                const email = res.data.response;
+                const liItem = document.createElement("li");
+                liItem.innerHTML = email;
+                emailList.appendChild(liItem);
+
+                console.log(email);
+            })
+            .catch(err => console.err(err));
+    };
+}
+
+generateButton.addEventListener("click", generateEmails);
+
