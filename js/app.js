b377198df4e55d522968de07e54fa382ff596ffb
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-05 16:31:43 +0100

    ADD: generating emails in list items through dom manipulation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
+const emailList = document.getElementById("email-list");
+
 for (let i = 0; i < 10; i++) {
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then(res => {
             const email = res.data.response;
+            const liItem = document.createElement("li");
+            liItem.innerHTML = email;
+            emailList.appendChild(liItem);
+
             console.log(email);
         })
         .catch(err => console.err(err));
